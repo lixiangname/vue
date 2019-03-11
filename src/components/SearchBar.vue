54e3cbed404e1cecb5dcef8c51a03cd9714e25a0
478893730 <478893730@qq.com>
2019-03-11 16:06:31 +0800

    添加一个搜索框

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,80 @@
+<template>
+
+	<div class="search-bar flex">
+		<div class="category"></div>
+		<div class="search-from flex-item">
+			<input type="" name="" id="" value="" />
+		
+		</div>
+		<div class="login"></div>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		data: function() {
+			return {};
+		},
+		methods: {},
+		props: [],
+		watch: {},
+		computed: {},
+		created: function() {},
+	}
+</script>
+
+<style>
+	.search-bar {
+		height: 44px;
+		background-color: red;
+	}
+	.search-from{
+		padding-top: 7px;
+	}
+	.search-bar .category,.search-bar .login{
+		width: 50px;
+		height: 44px;
+		background-color: blue;
+	}
+	.search-from input{
+		width: 100%;
+		height: 30px;
+		box-sizing: border-box;
+		border-radius: 15px;
+		background-color: #FFFFFF;
+	}
+	/*.search-bar {
+		height: 44px;
+		background-color: red;
+		position: relative;
+	}
+	.search-bar .category,.search-bar .login{
+		width: 50px;
+		height: 44px;
+		background-color: blue;
+		position: absolute;
+		top: 0px;
+		left: 0px;
+	}
+	
+	.search-bar .login{
+		right: 0px;
+		left: auto;
+	}
+	
+	.search-from{
+		width: 100%;
+		padding: 0px 50px;
+		box-sizing: border-box;
+		padding-top: 7px;
+	}
+	.search-from input{
+		width: 100%;
+		height: 30px;
+		box-sizing: border-box;
+		border-radius: 15px;
+		background-color: #FFFFFF;
+	}*/
+	
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+   <search-bar></search-bar>
+  </div>
+</template>
+
+<script>
+	
+import SearchBar from "@/components/SearchBar.vue";
+
+export default {
+  name: 'home',
+  components: {
+  	SearchBar:SearchBar
+  }
+}
+</script>
