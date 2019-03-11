34e8cafd88d9016c4c660d81d64214cd8f001689
478893730 <478893730@qq.com>
2019-03-11 16:34:19 +0800

    搜索框

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,50 +1,80 @@
 <template>
 
 	<div class="search-bar flex">
-		<div class="category"></div>
+		<div class="category">
+			<span class="icon"></span>
+		</div>
 		<div class="search-from flex-item">
+			<span class="icon"></span>
 			<input type="" name="" id="" value="" />
 		
 		</div>
-		<div class="login"></div>
+		<div class="login">
+			登录
+		</div>
 	</div>
 
 </template>
 
-<script>
-	export default {
-		data: function() {
-			return {};
-		},
-		methods: {},
-		props: [],
-		watch: {},
-		computed: {},
-		created: function() {},
-	}
-</script>
+<script>export default {
+	data: function() {
+		return {};
+	},
+	methods: {},
+	props: [],
+	watch: {},
+	computed: {},
+	created: function() {},
+}</script>
 
 <style>
-	.search-bar {
-		height: 44px;
-		background-color: red;
-	}
-	.search-from{
-		padding-top: 7px;
-	}
-	.search-bar .category,.search-bar .login{
-		width: 50px;
-		height: 44px;
-		background-color: blue;
-	}
-	.search-from input{
-		width: 100%;
-		height: 30px;
-		box-sizing: border-box;
-		border-radius: 15px;
-		background-color: #FFFFFF;
-	}
-	/*.search-bar {
+.search-bar {
+	height: 44px;
+	background-color: red;
+}
+.search-from {
+	padding-top: 7px;
+	position: relative;
+}
+.search-bar .category,
+.search-bar .login {
+	width: 50px;
+	height: 44px;
+}
+.search-from input {
+	width: 100%;
+	height: 30px;
+	padding-left: 68px;
+	box-sizing: border-box;
+	border-radius: 15px;
+	background-color: #FFFFFF;
+	border: none;
+}
+.search-bar .category .icon {
+	margin: 14px 0 0 15px;
+	width: 20px;
+	height: 18px;
+	display: block;
+	background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAgAgMAAAAdw9KTAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURUdwTP///////////waf0AoAAAADdFJOUwDjSYAlncUAAAAbSURBVBjTY5j/Hwq+MdTDmH+RmUgK6AuGhcsAU5tyB6Ji+x0AAAAASUVORK5CYII=) no-repeat;
+	background-size: 100% 100%;
+}
+.search-bar .login {
+	color: #fff;
+    font-size: 14px;
+    text-align: center;
+    line-height: 44px;
+}
+.search-from .icon{
+    width: 20px;
+    height: 15px;
+    background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAeBAMAAACs80HuAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAwUExURUdwTOk7Puo7PfE+SP9SUuo7Peo8Pu0/Qeo7Peo7Peo7Pek7Peo7Pes8Pus8P+k7PaLYf6IAAAAPdFJOUwCjwREGj1sg9Hrd6bFINxDM6d8AAAD3SURBVCjPY2AAAv3/nxnk////sUo1nQEOoIIgoOaARfD/WmyC/wNQBL+lzZQCCn5FEfwCZJX2//8/AV2Qoej//3YMQWb9/x8xBBkq/v8vwBBk/f8/AUOQ/f9/AQxBhvX/N2AKnv//AVMwHsjBEJz//zs2lVgE5bFpx2pR//8FUMEvcEE+mOPX/2+AC/JAvcnyH6gDJvj+/w8DEM0G0gEVZN8PDfpqkA6o4O3//5tBtGX//98OEEHmJGAkXWBgCNQC0t9Abv4aKrMewmYAR6sAAzyKfydABX8yIASFGSCCjQ/ggr+mg61WOjgXnHwmKSnpiM4FO5EBAIlo6/K7pNY1AAAAAElFTkSuQmCC) no-repeat;
+    background-size: 20px 15px;
+    margin: 8px 8px 0 15px;
+    position: absolute;
+    z-index: 1;
+    float: left;
+}
+/*.search-bar {
 		height: 44px;
 		background-color: red;
 		position: relative;
@@ -75,6 +105,4 @@
 		box-sizing: border-box;
 		border-radius: 15px;
 		background-color: #FFFFFF;
-	}*/
-	
-</style>
+	}*/</style>
